b90b5987ce0232536327dbc9e1a5969fd426f1ea
Raphael Tuishev <raf77786@gmail.com>
2022-06-08 01:05:47 +0300

    перенес стили в компонент

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,10 +1,21 @@
 import { FC } from 'react';
+import styled from 'styled-components';
+
+const HeaderStyle = styled.div`
+  h1 {
+    text-align: center;
+    font-size: 64px;
+    line-height: 78px;
+    margin: 0;
+    padding: 40px 0 40px 0;
+  }
+`;
 
 const Header: FC = () => {
   return (
-    <div>
+    <HeaderStyle>
       <h1>ONLY.</h1>
-    </div>
+    </HeaderStyle>
   );
 };
 
--- src/styles/AppStyle.ts
+++ src/styles/AppStyle.ts
@@ -7,10 +7,4 @@ export const AppStyle = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-
-  h1 {
-    font-size: 64px;
-    line-height: 78px;
-  }
 `;
